047c21e2aaba82157736982935f7f759aba03bb7
Aman Rastogi <amarasto@publicisgroupe.net>
2018-03-08 11:26:50 +0530

    two components created

--- src/app/sidebar-component/sidebar-component.component.ts
+++ src/app/sidebar-component/sidebar-component.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-sidebar-component',
   templateUrl: './sidebar-component.component.html',
@@ -10,6 +10,18 @@ export class SidebarComponentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      //var allPanels = $('.nav child_menu').hide();
+      
+      $('.menuItem').click(function() {
+      var  $this=$(this); 
+      $this.next().slideUp();
+      $this.next().slideToggle();
+      
+      }); 
+    });
+  
+
   }
 
 }
